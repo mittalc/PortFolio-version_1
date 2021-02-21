e0292aaa68861fd65da09bc3ded917ffbfedfc03
mittalc <55461354+mittalc@users.noreply.github.com>
2021-02-20 20:22:26 -0500

    fix bugs and enhance UI

---
+++ responsive.js
@@ -0,0 +1,9 @@
+<script>  function myFunction() {
+    var x = document.getElementById("showhide");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+      </script>
